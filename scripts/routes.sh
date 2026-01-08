26511571d5d31795f0bb1f693c84f534d6aa0c4b
Peter Duchnovsky <54581833+pduchnovsky@users.noreply.github.com>
2026-01-08 12:49:45 +0100

    upd

--- scripts/routes.sh
+++ scripts/routes.sh
@@ -9,11 +9,24 @@
 # @raycast.icon 🤖
 # @raycast.packageName routes
 
-s=10.10.10.2
-if netstat -nr -f inet | grep ^default | grep -q tun; then
-    sudo route delete $s 2>/dev/null
-    i=$(ifconfig | awk -v p="${s%.*}" '$1 ~ /^[a-z0-9]+:/ {i=$1; sub(":", "", i)} $1=="inet" && $2 ~ "^"p"." {print i; exit}')
-    [ -n "$i" ] && sudo route add -host $s -interface $i
-else
-    sudo route delete $s 2>/dev/null
+s="10.10.10.2"
+sudo route delete "$s" 2>/dev/null
+sleep 1
+if netstat -nr -f inet | grep '^default' | grep -qE 'tun|utun'; then
+    chosen_iface=$(ifconfig -a | awk '
+        /^[a-z0-9]+:/ {iface=$1; sub(":", "", iface); is_active=0; has_ip=0}
+        /status: active/ {is_active=1}
+        /inet 10\.10\.10/ {has_ip=1}
+        is_active && has_ip {
+            if (iface == "en10") { prioritized_iface = "en10" }
+            else if (iface == "en0") { non_prioritized_iface = "en0" }
+        }
+        END {
+            if (prioritized_iface) { print prioritized_iface }
+            else if (non_prioritized_iface) { print non_prioritized_iface }
+        }
+    ')
+    if [ -n "$chosen_iface" ]; then
+        sudo route add -host "$s" -interface "$chosen_iface"
+    fi
 fi
